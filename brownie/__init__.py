5db79e9c959149d1a5496f2dd419fa8cbd59babe
Daniel Neuhäuser <dasdasich@gmail.com>
2010-12-03 22:35:30 +0100

    Added docstring to package

---
+++ brownie/__init__.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+"""
+    brownie
+    ~~~~~~~
+
+    Brownie provides various different utilities and datastructures, which are
+    commonly used in Python applications.
+
+    :copyright: 2010 by Daniel Neuhäuser
+    :license: BSD, see LICENSE.rst for details
+"""
