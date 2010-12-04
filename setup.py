23a8104c286f3c39706c27e3ae86a2f4b6a990eb
Daniel Neuhäuser <dasdasich@gmail.com>
2010-12-04 18:31:38 +0100

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from setuptools import setup
+
+
+setup(
+    name='Brownie',
+    version='0.0+',
+    url='http://github.com/DasIch/brownie/',
+    license='BSD',
+    author='Daniel Neuhäuser',
+    author_email='dasdasich@gmail.com',
+    description='Common utilities and datastructures or Python applications.',
+    packages=['brownie'],
+    zip_safe=False,
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independant',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
