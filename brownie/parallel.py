7ab06e2d11a1c272e452a82a234a5efafd619bb2
Daniel Neuhäuser <dasdasich@gmail.com>
2010-12-19 18:36:15 +0100

    Remove unnecessary line

--- brownie/parallel.py
+++ brownie/parallel.py
@@ -13,8 +13,6 @@ import os
 try:
     from multiprocessing import cpu_count as get_cpu_count
 except ImportError:
-    MultiProcessingPool = None
-
     def get_cpu_count(default=None):
         sysconf_name = 'SC_NPROCESSORS_ONLN'
         if sysconf_name in os.sysconf_names:
