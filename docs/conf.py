163bdf156489fd7fd27257703d8a8d3769074da4
Daniel Neuhäuser <dasdasich@gmail.com>
2011-01-01 07:48:43 +0100

    Add Github Forkme ribbon to documentation (cherry picked from commit c53292f5abc615ed4a66c41589bb0b46928f13ac)

--- docs/conf.py
+++ docs/conf.py
@@ -100,7 +100,9 @@ html_theme = 'minimalism'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'github_fork': 'DasIch/Brownie'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_themes']
