62ec3c8dc84f99af23b296fd2bda31ca3471e167
Daniel Neuhäuser <dasdasich@gmail.com>
2011-01-12 22:18:21 +0100

    Fix runtest for the case that no arguments are given

--- runtests.py
+++ runtests.py
@@ -9,9 +9,11 @@ from brownie.importing import import_string
 def main(tests=sys.argv[1:]):
     prefix = 'brownie.tests.'
     if not tests:
-        tests = ['all']
-    for module in (import_string(prefix + test) for test in tests):
-        module.tests.run(reporter=FancyReporter(style='native'))
+        import_string(prefix + 'all').run(
+            reporter=FancyReporter(style='native')
+        )
+    for tests in (import_string(prefix + test + '.tests') for test in tests):
+        tests.run(reporter=FancyReporter(style='native'))
 
 
 if __name__ == '__main__':
