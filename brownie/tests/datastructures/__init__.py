e856a884e01388777e016e9c97fe2c4dbc7611d1
Daniel Neuhäuser <dasdasich@gmail.com>
2011-01-30 23:09:31 +0100

    Move TestOrderedSet to datastructures.sets

--- brownie/tests/datastructures/__init__.py
+++ brownie/tests/datastructures/__init__.py
@@ -21,7 +21,6 @@ from brownie.datastructures import (
     LazyList,
     CombinedSequence,
     CombinedList,
-    OrderedSet,
     MultiDict,
     ImmutableDict,
     ImmutableMultiDict,
@@ -35,6 +34,7 @@ from brownie.datastructures import (
     FixedDict,
     namedtuple
 )
+from brownie.tests.datastructures import sets
 from brownie.tests.datastructures import queues
 
 
@@ -1294,195 +1294,6 @@ class TestCombinedList(TestBase, CombinedSequenceTestMixin):
         Assert(bar) == [4, 5, 6]
 
 
-class TestOrderedSet(TestBase):
-    @test
-    def length(self):
-        Assert(len(OrderedSet([1, 2, 3]))) == 3
-
-    @test
-    def contains(self):
-        s = OrderedSet([1, 2, 3])
-        for element in s:
-            Assert(element).in_(s)
-        Assert(4).not_in(s)
-
-    @test
-    def add(self):
-        s = OrderedSet()
-        s.add(1)
-        Assert(1).in_(s)
-
-    @test
-    def remove(self):
-        s = OrderedSet()
-        with Assert.raises(KeyError):
-            s.remove(1)
-        s.add(1)
-        s.remove(1)
-
-    @test
-    def discard(self):
-        s = OrderedSet()
-        s.discard(1)
-        s.add(1)
-        s.discard(1)
-        Assert(1).not_in(s)
-
-    @test
-    def pop(self):
-        s = OrderedSet()
-        with Assert.raises(KeyError):
-            s.pop()
-        s = OrderedSet([1, 2, 3])
-        Assert(s.pop()) == 3
-        Assert(s.pop(last=False)) == 1
-
-    @test
-    def clear(self):
-        s = OrderedSet([1, 2, 3])
-        s.clear()
-        assert not s
-
-    @test
-    def update(self):
-        s = OrderedSet()
-        s.update('abc')
-        Assert(s) == OrderedSet('abc')
-
-    @test
-    def copy(self):
-        s = OrderedSet('abc')
-        Assert(s.copy()) == s
-        Assert(s.copy()).is_not(s)
-
-    @test
-    def inplace_update(self):
-        old = s = OrderedSet()
-        with Assert.raises(TypeError):
-            s |= 'abc'
-        s |= OrderedSet('abc')
-        Assert(s) == OrderedSet('abc')
-        Assert(s).is_(old)
-
-    @test
-    def issub_super_set(self):
-        a = OrderedSet('abc')
-        b = OrderedSet('abcdef')
-
-        a.issubset(a)
-        a.issuperset(a)
-        a.issubset(a)
-        a.issuperset(a)
-
-        assert a <= a
-        assert a >= a
-
-        assert a <= b
-        assert b >= a
-
-        assert not (a < a)
-        assert not (a > a)
-
-        assert a < b
-        assert not (a > b)
-
-    @test
-    def union(self):
-        a = OrderedSet('abc')
-        b = OrderedSet('def')
-        Assert(a.union('def', 'ghi')) == OrderedSet('abcdefghi')
-        Assert(a | b) == OrderedSet('abcdef')
-        with Assert.raises(TypeError):
-            a | 'abc'
-
-    @test
-    def intersection(self):
-        a = OrderedSet('abc')
-        Assert(a.intersection('ab', 'a')) == OrderedSet('a')
-        Assert(a & OrderedSet('ab')) == OrderedSet('ab')
-        with Assert.raises(TypeError):
-            a & 'ab'
-
-    @test
-    def intersection_update(self):
-        old = s = OrderedSet('abc')
-        with Assert.raises(TypeError):
-            s &= 'ab'
-        s &= OrderedSet('ab')
-        Assert(s) == OrderedSet('ab')
-        Assert(s).is_(old)
-
-    @test
-    def difference(self):
-        a = OrderedSet('abc')
-        Assert(a.difference('abc')) == OrderedSet()
-        Assert(a.difference('a', 'b', 'c')) == OrderedSet()
-        Assert(a - OrderedSet('ab')) == OrderedSet('c')
-        with Assert.raises(TypeError):
-            a - 'abc'
-
-    @test
-    def difference_update(self):
-        s = OrderedSet('abcd')
-        s -= s
-        Assert(s) == OrderedSet()
-
-        old = s = OrderedSet('abcd')
-        s -= OrderedSet('abc')
-        with Assert.raises(TypeError):
-            s -= 'abc'
-        Assert(s) == OrderedSet('d')
-        Assert(s).is_(old)
-
-    @test
-    def symmetric_difference(self):
-        for a, b in [('abc', 'def'), ('def', 'abc')]:
-            OrderedSet(a).symmetric_difference(b) == OrderedSet(a + b)
-            OrderedSet(a) ^ OrderedSet(b) == OrderedSet(a + b)
-
-            OrderedSet(a).symmetric_difference(a + b) == OrderedSet(b)
-            OrderedSet(a) ^ OrderedSet(a + b) == OrderedSet(b)
-
-        with Assert.raises(TypeError):
-            OrderedSet('abc') ^ 'def'
-
-    @test
-    def symmetric_difference_update(self):
-        old = s = OrderedSet('abc')
-        s ^= OrderedSet('def')
-        Assert(s) == OrderedSet('abcdef')
-        Assert(s).is_(old)
-        with Assert.raises(TypeError):
-            s ^= 'ghi'
-
-    @test
-    def iteration(self):
-        s = OrderedSet([1, 2, 3])
-        Assert(list(s)) == [1, 2, 3]
-        Assert(list(reversed(s))) == [3, 2, 1]
-
-    @test
-    def equality(self):
-        a = OrderedSet([1, 2, 3])
-        b = OrderedSet([3, 2, 1])
-        Assert(a) == a
-        Assert(a) == set(b)
-        Assert(b) == b
-        Assert(b) == set(a)
-        Assert(a) != b
-
-    @test
-    def hashability(self):
-        with Assert.raises(TypeError):
-            hash(OrderedSet())
-
-    @test
-    def repr(self):
-        Assert(repr(OrderedSet())) == 'OrderedSet()'
-        s = OrderedSet([1, 2, 3])
-        Assert(repr(s)) == 'OrderedSet([1, 2, 3])'
-
-
 class TestNamedTuple(TestBase):
     @test
     def string_field_names(self):
@@ -1556,7 +1367,7 @@ class TestNamedTuple(TestBase):
 tests = Tests([
     TestMissing, ImmutableDictTest, TestMultiDict, TestOrderedDict,
     TestCounter, TestLazyList, TestImmutableMultiDict, TestOrderedMultiDict,
-    TestImmutableOrderedMultiDict, TestOrderedSet, TestCombinedDict,
-    TestCombinedMultiDict, TestImmutableOrderedDict, TestNamedTuple,
-    TestFixedDict, TestCombinedSequence, TestCombinedList, queues.tests
+    TestImmutableOrderedMultiDict, TestCombinedDict, TestCombinedMultiDict,
+    TestImmutableOrderedDict, TestNamedTuple, TestFixedDict,
+    TestCombinedSequence, TestCombinedList, queues.tests, sets.tests
 ])
