85df3d2f42e0fd9ba63b4bbc37037c864e2155ee
Daniel Neuhäuser <dasdasich@gmail.com>
2011-02-05 19:18:02 +0100

    Change autodoc member order to bysource

--- docs/conf.py
+++ docs/conf.py
@@ -225,3 +225,5 @@ man_pages = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+autodoc_member_order = 'bysource'
