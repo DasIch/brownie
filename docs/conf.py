70aa7b7e1195b6ab653c1d17b5a89a7bb00e79da
Daniel Neuhäuser <dasdasich@gmail.com>
2011-02-13 21:07:50 +0100

    Mention 'others' in copyright of documentation

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Brownie'
-copyright = u'2010-2011, Daniel Neuhäuser'
+copyright = u'2010-2011, Daniel Neuhäuser and others'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
