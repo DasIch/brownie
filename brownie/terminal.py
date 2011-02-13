891f3cf27e66ba81a14fdd495adf3afbfdd9ebd7
Daniel Neuhäuser <dasdasich@gmail.com>
2011-02-13 17:52:08 +0100

    Add TerminalWriter.options
    This adds support for ANSI escape sequences for colours and text
    attributes (underline, bold, faint, blink, standout).
    
    The "test" results have to be checked visually at the moment, I will
    have to make this better in a way which allows easy comparisons.

--- brownie/terminal.py
+++ brownie/terminal.py
@@ -8,6 +8,7 @@
     :copyright: 2010 by Daniel Neuhäuser
     :license: BSD, see LICENSE.rst for details
 """
+from __future__ import with_statement
 import re
 import sys
 import codecs
@@ -15,6 +16,37 @@ from contextlib import contextmanager
 
 
 escape_char_re = re.compile('([%s])' % ''.join(map(chr, range(32) + [127])))
+
+
+_ansi_sequence = '\033[%sm'
+
+
+ATTRIBUTES = dict((key, _ansi_sequence % value) for key, value in [
+    ('reset','00'),
+    ('bold',  '01'),
+    ('faint', '02'),
+    ('standout', '03'),
+    ('underline', '04'),
+    ('blink', '05')
+])
+TEXT_COLOURS = {'reset': _ansi_sequence % '39'}
+BACKGROUND_COLOURS = {'reset': _ansi_sequence % '49'}
+_colour_names = [
+    ('black',      'darkgrey'),
+    ('darkred',    'red'),
+    ('darkgreen',  'green'),
+    ('darkyellow', 'yellow'),
+    ('darkblue',   'blue'),
+    ('purple',     'fuchsia'),
+    ('turquoise',  'teal'),
+    ('lightgray',  'white')
+]
+for i, (dark, light) in enumerate(_colour_names):
+    TEXT_COLOURS[dark] = _ansi_sequence % str(i + 30)
+    TEXT_COLOURS[light] = _ansi_sequence % ('%i;01' % (i + 30))
+
+    BACKGROUND_COLOURS[dark] = _ansi_sequence % str(i + 40)
+    BACKGROUND_COLOURS[light] = _ansi_sequence % ('%i;01' % (i + 40))
 
 
 def escape(string):
@@ -81,6 +113,8 @@ class TerminalWriter(object):
         #: The string used for indentation.
         self.indent_string = indent
 
+        self.optionstack = []
+
     def indent(self):
         """
         Indent the following lines with the given :attr:`indent`.
@@ -104,27 +138,172 @@ class TerminalWriter(object):
         finally:
             self.dedent()
 
-    def write(self, string):
+    def _get_options(self, kwargs):
+        result = {}
+        text_colour = kwargs.pop('text_colour', None)
+        background_colour = kwargs.pop('background_colour', None)
+        try:
+            if text_colour:
+                result['text_colour'] = TEXT_COLOURS[text_colour]
+            if background_colour:
+                result['background_colour'] = BACKGROUND_COLOURS[background_colour]
+        except KeyError, exc:
+            raise ValueError(*exc.args)
+        if kwargs:
+            used = []
+            attributes = []
+            for attr in kwargs:
+                if attr in ATTRIBUTES:
+                    attributes.append(ATTRIBUTES[attr])
+                    used.append(attr)
+            for attribute in used:
+                kwargs.pop(attribute)
+            result['attributes'] = attributes
+        return result
+
+    @contextmanager
+    def options(self, **kwargs):
+        """
+        A contextmanager which allows you to set certain text properties for
+        body.
+
+        Works only on terminals which implement ANSI escape sequences.
+
+        :param text_colour:
+            The desired text colour.
+
+        :param background_colour:
+            The desired background colour.
+
+        :param bold:
+            If present the text is displayed bold.
+
+        :param faint:
+            If present the text is displayed faint.
+
+        :param standout:
+            If present the text stands out.
+
+        :param blink:
+            If present the text blinks.
+
+        .. note::
+           The properties supported depend on the terminal, especially the
+           attributes (`bold`, `faint`, `standout` and `blink`) are not
+           necessarily available.
+
+        The following colours are available, the exact colour varies between
+        terminals and their configuration.
+
+        .. ansi-block::
+           :string_escape:
+
+           Dark       Light
+           ====       =====
+           \x1b[30mblack\x1b[0m      \x1b[30;01mdarkgrey\x1b[0m
+           \x1b[31mdarkred\x1b[0m    \x1b[31;01mred\x1b[0m
+           \x1b[32mdarkgreen\x1b[0m  \x1b[32;01mgreen\x1b[0m
+           \x1b[33mdarkyellow\x1b[0m \x1b[33;01myellow\x1b[0m
+           \x1b[34mdarkblue\x1b[0m   \x1b[34;01mblue\x1b[0m
+           \x1b[35mpurple\x1b[0m     \x1b[35;01mfuchsia\x1b[0m
+           \x1b[36mturquoise\x1b[0m  \x1b[36;01mteal\x1b[0m
+           \x1b[37mlightgray\x1b[0m  \x1b[37;01mwhite\x1b[0m
+        """
+        options = self._get_options(kwargs)
+        if kwargs:
+            raise TypeError('got unexpected argument')
+        self.optionstack.append(options)
+        self.apply_options(options)
+        try:
+            yield self
+        finally:
+            self.reset_options(options)
+            self.optionstack.pop()
+            if self.optionstack:
+                self.apply_options(self.optionstack[-1])
+
+    def apply_options(self, options):
+        text_colour = options.get('text_colour')
+        background_colour = options.get('background_colour')
+        attributes = options.get('attributes', [])
+
+        if text_colour:
+            self.stream.write(text_colour)
+        if background_colour:
+            self.stream.write(background_colour)
+        for attribute in attributes:
+            self.stream.write(attribute)
+
+    def reset_options(self, options):
+        if 'text_colour' in options:
+            self.stream.write(TEXT_COLOURS['reset'])
+        if 'background_colour' in options:
+            self.stream.write(BACKGROUND_COLOURS['reset'])
+        if 'attributes' in options:
+            self.stream.write(ATTRIBUTES['reset'])
+
+    def write(self, string, **options):
         """
         Writes the `given` string to the :attr:`stream`.
-        """
-        self.stream.write(string)
 
-    def writeline(self, line):
+        :param options:
+            Options for this operation, see :meth:`options`.
+        """
+        with self.options(**options):
+            self.stream.write(string)
+
+    def writeline(self, line, **options):
         """
         Writes the given `line` to the :attr:`stream` respecting indentation.
-        """
-        self.write(self.prefix + line + '\n')
 
-    def writelines(self, lines):
+        :param options:
+            Options for this operation, see :meth:`options`.
+        """
+        with self.options(**options):
+            self.write(self.prefix + line)
+        self.write('\n')
+
+    def writelines(self, lines, **options):
         """
         Writes each line in the given iterable to the :attr:`stream` respecting
         indentation.
+
+        :param options:
+            Options for this operation, see :meth:`options`.
         """
-        for line in lines:
-            self.writeline(line)
+        with self.options(**options):
+            for line in lines:
+                self.writeline(line)
 
     def __repr__(self):
         return '%s(%r, prefix=%r, indent=%r)' % (
             self.__class__.__name__, self.stream, self.prefix, self.indent_string
         )
+
+
+def main():
+    writer = TerminalWriter.from_bytestream(sys.stdout)
+    for dark, light in _colour_names:
+        writer.write(light, text_colour=light)
+        writer.write(' ')
+        writer.write(dark, text_colour=dark)
+        writer.write('\n')
+
+        writer.write(light, background_colour=light)
+        writer.write(' ')
+        writer.write(dark, background_colour=dark)
+        writer.write('\n')
+
+    for name in ATTRIBUTES:
+        writer.writeline(name, **{name: True})
+    with writer.options(underline=True):
+        writer.write('underline')
+        with writer.options(background_colour='red'):
+            writer.write('background')
+            writer.write('text', text_colour='green')
+            writer.write('background')
+        writer.write('underline')
+    writer.write('\n')
+
+if __name__ == '__main__':
+    main()
--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ sys.path.append(os.path.abspath('_themes/minimalism'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.viewcode',
-    'sphinx.ext.doctest'
+    'sphinx.ext.doctest', 'sphinxcontrib.ansi'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -230,3 +230,5 @@ intersphinx_mapping = {
 }
 
 autodoc_member_order = 'bysource'
+
+html_ansi_stylesheet = 'black-on-white.css'
